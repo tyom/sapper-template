ef6f389ea2a67350312df282d05455b23ae6908e
Tyom Semonov <inspiro@gmail.com>
2019-11-24 20:22:28 +0000

    Update GH workflows and build script to work with this repo

--- _template/build.sh
+++ _template/build.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "$0")"
 
 if [ "$CI" ]; then
-	(umask 0077; echo "$SSH_KEY" > ~/ssh_key; echo "$SSH_KEY_ROLLUP" > ~/ssh_key_rollup; echo "$SSH_KEY_WEBPACK" > ~/ssh_key_webpack)
-	git config user.email 'noreply@svelte.dev'
+	(umask 0077; echo "$SSH_KEY" > ~/ssh_key)
+	git config user.email 'noreply@tyom.dev'
 	git config user.name '[bot]'
 fi
 
@@ -14,6 +14,4 @@ WEBPACK=webpack
 ./create-branches.sh $ROLLUP $WEBPACK
 
 # force push rollup and webpack branches and repos
-GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key' git push git@github.com:sveltejs/sapper-template.git $ROLLUP $WEBPACK -f
-GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_rollup' git push git@github.com:sveltejs/sapper-template-rollup.git $ROLLUP:master -f
-GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_webpack' git push git@github.com:sveltejs/sapper-template-webpack.git $WEBPACK:master -f
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key' git push git@github.com:tyom/sapper-template.git $ROLLUP $WEBPACK -f
