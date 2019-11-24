919f1e995a9951f4aa8625a5e0ef1381e258095f
Tyom Semonov <inspiro@gmail.com>
2019-11-24 15:06:47 +0000

    Fix ESLint errors based on new config

--- _template/build-pkg.js
+++ _template/build-pkg.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
+const fs = require('fs')
+const { join } = require('path')
 
-const type = process.argv[2];
+const type = process.argv[2]
 
-const pkg = require('../package_template.json');
+const pkg = require('../package_template.json')
 for (const key in pkg['merge-configs'][type]) {
-	Object.assign(pkg[key], pkg['merge-configs'][type][key]);
+  Object.assign(pkg[key], pkg['merge-configs'][type][key])
 }
-delete pkg['merge-configs'];
+delete pkg['merge-configs']
 fs.writeFileSync(
-	__dirname + '/../package.json',
-	JSON.stringify(pkg, null, '  ') + '\n'
-);
+  join(__dirname, '/../package.json'),
+  JSON.stringify(pkg, null, '  ') + '\n',
+)
